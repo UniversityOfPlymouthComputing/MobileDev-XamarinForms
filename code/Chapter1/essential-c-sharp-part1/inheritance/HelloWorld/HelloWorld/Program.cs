4007d00b84ffa114d37435ee50dbbb9ab53a551e
noutram <nicholas.outram@plymouth.ac.uk>
2019-07-17 15:21:11 +0100

    reshuffle

---
+++ code/Chapter1/essential-c-sharp-part1/inheritance/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DepartmentOfTransport;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Code running: Project version " + RoadVehicle.ProjectVersion);
+
+            RoadVehicle v1 = new RoadVehicle(EngineSerialNumber:12345);
+            Console.WriteLine(v1.Description);
+
+            RoadVehicle v2 = new RoadVehicle(EngineSerialNumber:2468);
+            Console.WriteLine(v1.Description);
+            Console.WriteLine(v1.Description);
+
+            Console.WriteLine("Serial " + v1.EngineSerialNumber);
+        }
+    }
+}
