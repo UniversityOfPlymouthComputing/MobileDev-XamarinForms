68c5bfe53ebf2bd93bd66269b1612add7f3036f0
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2019-07-20 09:12:53 +0100

    binding context set in XAML

---
+++ code/Chapter 2/Bindings/HelloBindings-06/HelloBindings/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloBindings
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        //MainPageViewModel ViewModel = new MainPageViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //BindingContext = new MainPageViewModel();
+
+            //Bindings
+            //ToggleSwitch.BindingContext = ViewModel;
+            ToggleSwitch.SetBinding(Switch.IsToggledProperty, "UIVisible", BindingMode.OneWayToSource);
+
+            //MessageButton.BindingContext = ViewModel;
+            //MessageButton.SetBinding(Button.IsEnabledProperty, "UIVisible", BindingMode.OneWay);
+            MessageButton.SetBinding(Button.TextProperty, "SayingNumber", BindingMode.OneWay, null, "Saying: {0:d}");
+            MessageButton.SetBinding(Button.CommandProperty, "ButtonCommand"); //Cannot work as Model has no such (Forms aware) property
+
+            //MessageLabel.BindingContext = ViewModel;
+            MessageLabel.SetBinding(Label.TextProperty, "CurrentSaying", BindingMode.OneWay);
+            MessageLabel.SetBinding(Label.IsVisibleProperty, "UIVisible", BindingMode.OneWay);
+            MessageLabel.SetBinding(Label.TextColorProperty, "SayingNumber", BindingMode.OneWay, new ColorConverter());
+
+        }
+
+    }
+}
---
+++ code/Chapter 2/Bindings/HelloBindings-06/HelloBindings/Model.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace HelloBindings
+{
+    class Model //: INotifyPropertyChanged
+    {
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private List<string> Sayings = new List<string>
+        {
+            "May the Force be With You",
+            "Live long and prosper",
+            "Nanoo nanoo"
+        };
+
+        //Index of which saying to use
+        public int SayingNumber { get; private set; }
+        public string Message { get; private set;  }
+        public void NextMessage()
+        {
+            SayingNumber = (SayingNumber + 1) % Sayings.Count;
+            Message = Sayings[SayingNumber];
+        }
+
+        public Model()
+        {
+            Message = Sayings[0];
+        }
+    }
+}
