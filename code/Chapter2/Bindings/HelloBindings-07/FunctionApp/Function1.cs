4c248c9d7ce650e2a526d1e0b38f61ec0e9a2e75
noutram <nicholas.outram@plymouth.ac.uk>
2019-07-30 15:55:48 +0100

    Update Function1.cs

--- code/Chapter2/Bindings/HelloBindings-07/FunctionApp/Function1.cs
+++ code/Chapter2/Bindings/HelloBindings-07/FunctionApp/Function1.cs
@@ -1,11 +1,7 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using HelloBindingsLib;
 
@@ -20,31 +16,27 @@ namespace FunctionApp
             "Nanoo nanoo",
             "Make it So!"
         };
-        private static int Count => Function1.Sayings.Count;
+        private static int Count => Sayings.Count;
 
         [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "LookupSaying")] HttpRequest req,
-            ILogger log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "LookupSaying")] HttpRequest req)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
             string name = req.Query["index"];
             bool success = int.TryParse(name, out int index);
             if (success)
             {
-                if ((index >= 0) && (index < Function1.Count))
+                if ((index >= 0) && (index < Count))
                 {
                     PayLoad p = new PayLoad
                     {
                         From = Count,
                         Saying = Sayings[index]
                     };
-                    return (ActionResult)new OkObjectResult(p.ToXML());
+                    return new OkObjectResult(p.ToXML());
                 }
                 else
                 {
-                    return new BadRequestObjectResult("Index out of range. Please use an index from 0.." + Count);
+                    return new BadRequestObjectResult("Index out of range. Please use an index from 0.." + (Count-1));
                 }
 
             } else
