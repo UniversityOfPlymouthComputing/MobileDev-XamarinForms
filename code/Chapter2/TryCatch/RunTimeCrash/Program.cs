2f35b1146e15698aa61c8c799c49a4c023f7ad9a
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2019-10-03 06:59:18 +0100

    more try-catch examples

---
+++ code/Chapter2/TryCatch/RunTimeCrash/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TryCatch
+{
+    class Program
+    {
+        public Program()
+        {
+            Dictionary<string, uint> lookup = new Dictionary<string, uint>();
+
+            //Add is used for new items that do not already exist
+            lookup.Add("Life", 42);
+            lookup.Add("Loudest", 10);
+            lookup.Add("LouderStill", 11);
+
+            // ********************************
+            // CASE 1 - null argument exception
+            // lookup.Add(null, 1066);
+            // ********************************
+
+            //Does not throw an exception
+            if (lookup.TryAdd("Life", 21) == false)
+            {
+                Console.WriteLine("Was not able to replicate the `Life` key");
+            }
+
+            // ********************************
+            // CASE 2 - duplicate key
+            // lookup.Add("Life", 21);
+            // ********************************
+
+            //Assumes key exists - throws exception if not
+            uint u = lookup["Life"];
+
+            //No exception thrown - check performed by developer
+            if (lookup.TryGetValue("Life", out uint w))
+            {
+                Console.WriteLine($"The answer..{w}");
+            }
+
+            // ********************************
+            // CASE 3 - Key does not exist
+            // v = lookup["LifeOnVenus"];
+            // ********************************
+
+
+            uint uu = 1; uint vv = 10;
+            // ********************************
+            // CASE 4 - Integer Divide by Zero
+            //uint y = vv / (uu - 1);
+            // ********************************
+
+            Console.WriteLine("***********Done***********");
+        }
+
+        static void Main(string[] args)
+        {
+            _ = new Program();
+        }
+    }
+}
