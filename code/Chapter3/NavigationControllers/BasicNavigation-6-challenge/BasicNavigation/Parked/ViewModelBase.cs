1809f0f1d810c0ad15a1676575adf8551e6bf475
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2019-11-13 21:15:45 +0000

    factored out baseclasses for model and VM

---
+++ code/Chapter3/NavigationControllers/BasicNavigation-6-challenge/BasicNavigation/Parked/ViewModelBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace BasicNavigation
+{
+
+    public abstract class ViewModelBase<ModelType> : INotifyPropertyChanged where ModelType : ModelBase
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ModelType model;
+        public ModelType Model {
+            get => model;
+            set
+            {
+                if (model != value)
+                {
+                    model = value;
+                    model.PropertyChanged += OnModelPropertyChanged;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        protected abstract void OnModelPropertyChanged(object sender, PropertyChangedEventArgs e);
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ViewModelBase(ModelType m = null)
+        {
+            if (m != null)
+            {
+                Model = m;
+            }
+        }
+
+        protected INavigation Navigation
+        {
+            get => Application.Current.MainPage.Navigation;
+        }
+
+    }
+}
