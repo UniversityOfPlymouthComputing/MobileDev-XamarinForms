8d0a9ad0989a3c514c7c9d8140158c5a62f9c217
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2019-11-21 16:35:14 +0000

    ok - draft 1 complete

--- code/Chapter3/TabControllers/PhoneFeatureApp/PhoneFeatureApp/Location/LocationViewModel.cs
+++ code/Chapter3/TabControllers/PhoneFeatureApp/PhoneFeatureApp/Location/LocationViewModel.cs
@@ -2,14 +2,127 @@
 using MVVMBase;
 using Xamarin.Forms;
 using Xamarin.Essentials;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using System.Threading.Tasks;
+
+// see https://docs.microsoft.com/en-us/xamarin/essentials/geolocation?context=xamarin%2Fxamarin-forms&tabs=android
 
 namespace PhoneFeatureApp.Location
 {
     public class LocationViewModel : ViewModelCommon
     {
+
+        private string locationString;
+        private ICommand buttonCommand;
+        private ICommand mapCommand;
+        private bool isBusy = false;
+        private Xamarin.Essentials.Location loc = null;
+
+        // ********************** Non-Bindable properties **********************
+        public Xamarin.Essentials.Location Loc {
+            get => loc;
+            private set
+            {
+                if (loc != value)
+                {
+                    loc = value;
+                    (MapCommand as Command).ChangeCanExecute();
+                }
+            }
+        }
+
+        // ********************** Bindable properties **********************
+        public string LocationString {
+            get => locationString;
+            set
+            {
+                if (locationString != value)
+                {
+                    locationString = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand ButtonCommand { get => buttonCommand; private set => buttonCommand = value; }
+
+        public ICommand MapCommand { get => mapCommand; set => mapCommand = value; }
+
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                if (isBusy != value)
+                {
+                    isBusy = value;
+                    OnPropertyChanged();
+                    (ButtonCommand as Command).ChangeCanExecute();
+                    (MapCommand as Command).ChangeCanExecute();
+                }
+            }
+        }
+
+        // ************************* Constructor **************************
         public LocationViewModel()
         {
             subscribeToBackgroundColChange();
+
+            ButtonCommand = new Command(
+                execute: async () =>
+                {
+                    try
+                    {
+                        IsBusy = true;
+                        GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Best);
+                        Loc = await Geolocation.GetLocationAsync(request);
+                        IsBusy = false;
+                        if (Loc != null)
+                        {
+                            LocationString = string.Format("Lat: {0:F3}, Long: {1:F3}, Alt: {2:F3}",
+                                Loc.Latitude, Loc.Longitude, Loc.Altitude);
+                        }
+                    }
+                    catch (FeatureNotSupportedException)
+                    {
+                        // Handle not supported on device exception
+                        LocationString = "Feature not supported";
+                    }
+                    catch (FeatureNotEnabledException)
+                    {
+                        // Handle not enabled on device exception
+                        LocationString = "Location not enabled";
+                    }
+                    catch (PermissionException)
+                    {
+                        // Handle permission exception
+                        LocationString = "Permission Denied";
+                    }
+                    catch (Exception)
+                    {
+                        // Unable to get location
+                        LocationString = "Error - cannot get location";
+                    }
+                },
+                canExecute: () => { return !IsBusy;  }
+                );
+
+            MapCommand = new Command(
+                execute: async () =>
+                {
+                    var options = new MapLaunchOptions { Name = "Current Location" };
+                    await Map.OpenAsync(Loc, options);
+                },
+                canExecute: () => 
+                {
+                    return (Loc != null) && (IsBusy == false);
+                } );
+
         }
+
+        // **************************** EVENTS *****************************
+
+
     }
 }
