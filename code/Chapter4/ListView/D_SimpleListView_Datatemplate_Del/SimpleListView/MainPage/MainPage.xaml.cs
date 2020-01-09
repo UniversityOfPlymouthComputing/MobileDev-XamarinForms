47a25a2a5fd94c89467e9f61be01c482f2a45bb3
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2020-01-09 10:19:02 +0000

    Datatemplates

---
+++ code/Chapter4/ListView/D_SimpleListView_Datatemplate_Del/SimpleListView/MainPage/MainPage.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using MVVMBase;
+using Xamarin.Forms;
+
+namespace SimpleListView
+{
+
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage, IMainPageHelper
+    {
+        private MainPageViewModel vm;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Set binding context
+            vm = new MainPageViewModel(this);
+            BindingContext = vm;
+
+            // Hook up event handlers
+            PlanetListView.SelectionMode = ListViewSelectionMode.Single;        //Default
+            PlanetListView.ItemTapped += PlanetListView_ItemTapped;             //User tapped
+            PlanetListView.ItemSelected += PlanetListView_ItemSelected;    //Selection changed
+
+            // *****************************************************************
+            // DATA TEMPLATE
+            // *****************************************************************
+
+            //Data template will instantiate a cell "when required"
+
+            //Two options: I like the second but the first is more common
+
+            //DataTemplate DataTemplate = new DataTemplate(typeof(TextCell)); 
+            DataTemplate dataTemplate = new DataTemplate( () =>
+            {
+                //Return a subclass of Cell
+                TextCell cell = new TextCell();
+                //We can set properties on cell
+                MenuItem m1 = new MenuItem
+                {
+                    Text = "Delete",
+                    IsDestructive = true
+                };
+                
+                m1.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+                //m1.Clicked += M1_Clicked;
+                m1.Clicked += (object sender, System.EventArgs e) => {
+                    //Note that conditional statements evaluate left to right - && will short-circuit if the first condition is not true
+                    if ((sender is MenuItem mi) && (mi.CommandParameter is SolPlanet p))
+                    {
+                        Debug.WriteLine($"Vogons are destroying {p.Name}");
+                        vm.DeleteItem(p);
+                    }
+                };
+                
+                //Add menu item to the cell
+                cell.ContextActions.Add(m1);
+
+                return cell;
+            });
+
+            //Binding proxy: When the DataTemplate instantiates a cell, it will also set up the binding as specified below
+            dataTemplate.SetBinding(TextCell.TextProperty, "Name");
+            dataTemplate.SetBinding(TextCell.DetailProperty, "Distance");
+
+            //Finally, set the ItemTemplate property (type DataTemplate)
+            PlanetListView.ItemTemplate = dataTemplate;
+            // *****************************************************************
+        }
+
+        /*
+        private void M1_Clicked(object sender, System.EventArgs e)
+        {
+            //Note that conditional statements evaluate left to right - && will short-circuit if the first condition is not true
+            if ((sender is MenuItem mi) && (mi.CommandParameter is SolPlanet p))
+            {
+                Debug.WriteLine($"Vogons are destroying {p.Name}");
+                vm.DeleteItem(p);
+            }
+        }
+        */
+
+        //Called if the item selection changes (by any means)
+        private void PlanetListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            //If nothing is selected, there is nothing to do
+            if (e.SelectedItem == null) return;
+
+            //Extract data
+            SolPlanet item = (SolPlanet)e.SelectedItem;
+            int selectedRow = e.SelectedItemIndex;
+
+            //Update ViewModel
+            vm.ItemSelectionChanged(row: selectedRow, planet: item);
+        }
+
+        //Called if user taps a row (as opposed to programatically changing the selection)
+        private void PlanetListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            SolPlanet item = (SolPlanet)e.Item;
+            int selectedRow = e.ItemIndex;
+            vm.UserTappedList(row: selectedRow, planet: item);
+        }
+
+        INavigation IPage.NavigationProxy => Navigation;
+
+        public async Task TextPopup(string title, string message)
+        {
+            await DisplayAlert(title, message, "Dismiss");
+        }
+
+        public void ScrollToObject(object obj)
+        {
+            PlanetListView.ScrollTo(obj, ScrollToPosition.End, true);
+        }
+    }
+}
---
+++ code/Chapter4/ListView/D_SimpleListView_Datatemplate_XAML/SimpleListView/MainPage/MainPageViewModel.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MVVMBase;
+using Xamarin.Forms;
+
+
+// https://docs.microsoft.com/xamarin/xamarin-forms/user-interface/listview/
+// https://docs.microsoft.com/xamarin/xamarin-forms/user-interface/listview/interactivity
+// https://docs.microsoft.com/dotnet/api/xamarin.forms.listview?view=xamarin-forms
+
+namespace SimpleListView
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        //**********************  PRIVATE MEMBER VARIABLES *********************
+
+        private IMainPageHelper _viewHelper;
+        private ObservableCollection<SolPlanet> _planets; // Implements INotifyCollectionChanged
+        private bool _selectionModeOn = true;
+        private string _titleString = "Nothing Selected";
+        private SolPlanet _selectedPlanet;
+        private int _selectedRow = 0;
+        private int _selectionCount = 0;
+        private int _tapCount = 0;
+
+        // ***********************  BINDABLE PROPERTIES ************************
+
+        // ItemSource binds to an IEnumerable
+        public ObservableCollection<SolPlanet> Planets
+        {
+            get => _planets;
+            set
+            {
+                if (_planets == value) return;
+                _planets = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool SelectionModeOn {
+            get => _selectionModeOn;
+            set
+            {
+                if (_selectionModeOn == value) return;
+
+                _selectionModeOn = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string TitleString {
+            get => _titleString ?? "Nothing Selected";
+            set
+            {
+                if (_titleString == value) return;
+                _titleString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //This property is updated if the ListView selection changes by any means but ONLY if the selection changes
+        public SolPlanet SelectedPlanet
+        {
+            get => _selectedPlanet;
+            set
+            {
+                if (_selectedPlanet == value) return;
+
+                _selectedPlanet = value;
+
+                //Update UI
+                TitleString = _selectedPlanet?.Name ?? "Nothing Selected";
+            }
+        }
+
+        public int SelectedRow
+        {
+            get => _selectedRow;
+            set
+            {
+                if (_selectedRow == value) return;
+                _selectedRow = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int SelectionCount {
+            get => _selectionCount;
+            set
+            {
+                if (_selectionCount == value) return;
+                _selectionCount = value;
+                OnPropertyChanged(nameof(CounterString));
+            }
+        }
+
+        public int TapCount {
+            get => _tapCount;
+            set
+            {
+                if (_tapCount == value) return;
+                _tapCount = value;
+                OnPropertyChanged(nameof(CounterString));
+            }
+        }
+
+        public string CounterString => $"Taps: {TapCount}, Selections: {SelectionCount}";
+
+        public ICommand DeleteCommand { get; private set; }
+
+        // **************************  EVENT HANDLERS **************************
+
+        //Event handler for user tap
+        public void UserTappedList(int row, SolPlanet planet)
+        {
+            SelectedRow = row;
+            TapCount += 1;
+            string item = $"{planet.Name} - Row {row} tapped";
+            Planets.Add(new SolPlanet(item, planet.Distance));
+            _viewHelper.ScrollToObject(item);
+        }
+
+        //Event handler for selection changed
+        public void ItemSelectionChanged(int row, SolPlanet planet)
+        {
+            SelectedRow = row;
+            SelectionCount += 1;
+        }
+
+        //Menu item event - delete
+        public void DeleteItem(SolPlanet p) => Planets.Remove(p);
+
+        // ***************************  CONSTRUCTOR ****************************
+        public MainPageViewModel(IMainPageHelper viewHelper) : base(viewHelper.NavigationProxy)
+        {
+            _viewHelper = viewHelper;
+
+            Planets = new ObservableCollection<SolPlanet>()
+            {
+                new SolPlanet("Earth", 147.1),
+                new SolPlanet("Mercury", 69.543),
+                new SolPlanet("Venus", 108.62),
+                new SolPlanet("Jupiter", 782.32),
+                new SolPlanet("Mars", 238.92),
+                new SolPlanet("Saturn", 1498.3),
+                new SolPlanet("Pluto", 5906.4)
+            };
+
+            DeleteCommand = new Command<SolPlanet>(execute: (p) => DeleteItem(p));
+        }
+
+    } //END OF CLASS
+} //END OF NAMESPACE
