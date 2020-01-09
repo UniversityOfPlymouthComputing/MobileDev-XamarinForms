7ca7cbfcd4962be76b38d09a62eb91646034f1eb
noutram <nicholas.outram@plymouth.ac.uk>
2020-01-09 16:15:10 +0000

    XAML nailed

--- code/Chapter4/ListView/E_SimpleListView_Datatemplate_XAML/SimpleListView/MainPage/MainPage.xaml.cs
+++ code/Chapter4/ListView/E_SimpleListView_Datatemplate_XAML/SimpleListView/MainPage/MainPage.xaml.cs
@@ -13,6 +13,7 @@ namespace SimpleListView
     public partial class MainPage : ContentPage, IMainPageHelper
     {
         private MainPageViewModel vm;
+
         public MainPage()
         {
             InitializeComponent();
@@ -25,76 +26,18 @@ namespace SimpleListView
             PlanetListView.SelectionMode = ListViewSelectionMode.Single;        //Default
             PlanetListView.ItemTapped += PlanetListView_ItemTapped;             //User tapped
             PlanetListView.ItemSelected += PlanetListView_ItemSelected;    //Selection changed
-
-            // *****************************************************************
-            // DATA TEMPLATE
-            // *****************************************************************
-
-        /*
-            //DataTemplate DataTemplate = new DataTemplate(typeof(TextCell)); 
-            DataTemplate dataTemplate = new DataTemplate( () =>
-            {
-                //Return a subclass of Cell
-                TextCell cell = new TextCell();
-                //We can set properties on cell
-                MenuItem m1 = new MenuItem
-                {
-                    Text = "Delete",
-                    IsDestructive = true
-                };
-                
-                m1.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
-                //m1.Clicked += M1_Clicked;
-                m1.Clicked += (object sender, System.EventArgs e) => {
-                    //Note that conditional statements evaluate left to right - && will short-circuit if the first condition is not true
-                    if ((sender is MenuItem mi) && (mi.CommandParameter is SolPlanet p))
-                    {
-                        Debug.WriteLine($"Vogons are destroying {p.Name}");
-                        vm.DeleteItem(p);
-                    }
-                };
-                
-                //Add menu item to the cell
-                cell.ContextActions.Add(m1);
-
-                return cell;
-            });
-
-            //Binding proxy: When the DataTemplate instantiates a cell, it will also set up the binding as specified below
-            dataTemplate.SetBinding(TextCell.TextProperty, "Name");
-            dataTemplate.SetBinding(TextCell.DetailProperty, "Distance");
-            
-
-            //Finally, set the ItemTemplate property (type DataTemplate)
-            PlanetListView.ItemTemplate = dataTemplate;
-        */
-            // *****************************************************************
         }
-
-        
-        private void M1_Clicked(object sender, System.EventArgs e)
-        {
-            //Note that conditional statements evaluate left to right - && will short-circuit if the first condition is not true
-            if ((sender is MenuItem mi) && (mi.CommandParameter is SolPlanet p))
-            {
-                Debug.WriteLine($"Vogons are destroying {p.Name}");
-                vm.DeleteItem(p);
-            }
-        }
-        
 
         //Called if the item selection changes (by any means)
         private void PlanetListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            //If nothing is selected, there is nothing to do
-            if (e.SelectedItem == null) return;
-
             //Extract data
-            SolPlanet item = (SolPlanet)e.SelectedItem;
-            int selectedRow = e.SelectedItemIndex;
-
-            //Update ViewModel
-            vm.ItemSelectionChanged(row: selectedRow, planet: item);
+            if (e.SelectedItem is SolPlanet item)
+            {
+                //Update ViewModel
+                int selectedRow = e.SelectedItemIndex;
+                vm.ItemSelectionChanged(row: selectedRow, planet: item);
+            }
         }
 
         //Called if user taps a row (as opposed to programatically changing the selection)
