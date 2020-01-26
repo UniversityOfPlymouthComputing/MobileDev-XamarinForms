385597332e1de3f66601eaaa879ff12ffb24df9a
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2020-01-26 07:59:10 +0000

    Started template selector

---
+++ code/Chapter4/ListView/J_SimpleListView_DatatemplateSel/SimpleListView/MainPage/SolPlanet.cs
@@ -0,0 +1,13 @@
+﻿// https://docs.microsoft.com/xamarin/xamarin-forms/user-interface/listview/
+// https://docs.microsoft.com/xamarin/xamarin-forms/user-interface/listview/interactivity
+// https://docs.microsoft.com/dotnet/api/xamarin.forms.listview?view=xamarin-forms
+
+namespace SimpleListView
+{
+    public class SolPlanet
+    {
+        public string Name { get; set; }
+        public double Distance { get; set; }
+        public SolPlanet(string name, double dist) => (Name, Distance) = (name, dist);
+    }
+} //END OF NAMESPACE
