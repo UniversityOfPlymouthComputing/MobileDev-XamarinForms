ff186c69dfb05550e50e4675d084b240e549c0d3
noutram <nicholas.outram@plymouth.ac.uk>
2020-01-29 10:00:35 +0000

    HelloWorld Demo Added

---
+++ code/Chapter1/HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //MessageButton.Clicked += MessageButton_Clicked;
+        }
+
+        private void MessageButton_Clicked(object sender, EventArgs e)
+        {
+            MessageLabel.Text = "Hello World";
+        }
+    }
+}
