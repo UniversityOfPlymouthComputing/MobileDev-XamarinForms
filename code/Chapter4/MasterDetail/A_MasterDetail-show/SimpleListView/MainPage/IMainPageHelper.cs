6aa069a59d31a08b3aedf563318f74fb850e0b1e
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2020-02-02 08:30:15 +0000

    restructure

---
+++ code/Chapter4/MasterDetail/A_MasterDetail-show/SimpleListView/MainPage/IMainPageHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using MVVMBase;
+
+namespace SimpleListView
+{
+    public interface IMainPageHelper : IPage
+    {
+        Task TextPopup(string title, string message);
+        void ScrollToObject(object obj);
+    }
+
+    
+}
---
+++ code/Chapter4/MasterDetail/A_MasterDetail-show/SimpleListView/MainPage/PlanetTemplateSelector.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace SimpleListView
+{
+    public class PlanetTemplateSelector : DataTemplateSelector
+    {
+        public ContentPage PageRef { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            DataTemplate template;
+            ListView list = (ListView)container;
+
+            if (item is SolPlanet p)
+            {
+                MenuItem m1 = new MenuItem
+                {
+                    Text = "Delete",
+                    IsDestructive = true,
+                };
+                m1.SetBinding(MenuItem.CommandProperty, new Binding("DeleteCommand", source: PageRef.BindingContext));
+                m1.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+
+                MenuItem m2 = new MenuItem
+                {
+                    Text = "Swap",
+                    IsDestructive = false,
+                };
+                m2.SetBinding(MenuItem.CommandProperty, new Binding("SwapCommand", source: PageRef.BindingContext));
+                m2.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+
+                if (p.Name == "Earth")
+                {
+                    //No item template for Earth - cannot delete or move
+                    template = new DataTemplate(typeof(HomePlanetViewCell));
+                }
+                else
+                {
+                    template = new DataTemplate(() =>
+                    {
+                        PlanetViewCell cell = new PlanetViewCell();
+                        cell.ContextActions.Add(m1);
+                        cell.ContextActions.Add(m2);
+                        return cell;
+                    });
+                }
+            }
+            else
+            {
+                template = new DataTemplate(typeof(TextCell));
+            }
+
+            return template;
+        }
+    }
+}
