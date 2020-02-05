d5bad91269712839df1393e8e692d8674e4944f6
noutram <nicholas.outram@plymouth.ac.uk>
2020-02-05 12:22:16 +0000

    stuff

---
+++ code/Chapter1/LectureDemos/EventsAndBindings/EventsAndBindings/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EventsAndBindings
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private int _sliderValue = 1;
+        public int SliderValue
+        {
+            get
+            {
+                return _sliderValue;
+            }
+            set
+            {
+                if (_sliderValue == value) return;
+                _sliderValue = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+            /*
+            TextLabel.BindingContext = this.MySlider;
+            TextLabel.SetBinding(Label.TextProperty, "Value");
+            */
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int v = new Random().Next(1, 11);
+            SliderValue = v;
+        }
+    }
+}
