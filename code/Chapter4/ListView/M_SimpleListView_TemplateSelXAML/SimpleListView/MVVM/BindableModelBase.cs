9556e414a44514c5831e04f75351bf315a1e6cbc
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2020-02-07 16:57:49 +0000

    Created data templates in XAML

---
+++ code/Chapter4/ListView/M_SimpleListView_TemplateSelXAML/SimpleListView/MVVM/BindableModelBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace MVVMBase
+{
+    public class BindableModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected string filename;
+        public string Filename { get => filename; set => filename = value; }
+
+        //Create events when properties change
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //Serialise this instance to an XML file
+        public void Save()
+        {
+            if (Filename != null)
+            {
+                Save(Filename);
+            }
+            else
+            {
+                throw new Exception("No filename set - either load or save with filename first");
+            }
+        }
+
+        public void Save(string FileName)
+        {
+            this.Filename = Filename;
+            using (var writer = new System.IO.StreamWriter(FileName))
+            {
+                var serializer = new XmlSerializer(this.GetType());
+                serializer.Serialize(writer, this);
+                writer.Flush();
+            }
+        }
+
+        //Deserialise an XML file to a new instance of this type
+        public static ModelType Load<ModelType>(string fn) where ModelType : BindableModelBase
+        {
+            try
+            {
+                using (FileStream stream = File.OpenRead(fn))
+                {
+                    var serializer = new XmlSerializer(typeof(ModelType));
+                    ModelType m = serializer.Deserialize(stream) as ModelType;
+                    m.Filename = fn;
+                    return m;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ code/Chapter4/ListView/M_SimpleListView_TemplateSelXAML/SimpleListView/MainPage/PlanetTemplateSelector.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace SimpleListView
+{
+    public class PlanetTemplateSelector : DataTemplateSelector
+    {
+        //public ContentPage PageRef { get; set; }
+
+        //Set through bindings
+        public DataTemplate EarthTemplate { get; set; }
+        public DataTemplate OtherplanetTemplate { get; set; }
+
+        //Selector
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            //ListView list = (ListView)container;
+
+            if (item is SolPlanet p)
+            {
+                if (p.Name == "Earth")
+                {
+                    return EarthTemplate;
+                }
+                else
+                {
+                    return OtherplanetTemplate;
+                }
+            }
+            else
+            {
+                return new DataTemplate(typeof(TextCell));
+            }
+        }
+    }
+}
