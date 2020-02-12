770e126865e74ef36c3947674061eba746840e69
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2020-02-12 10:39:31 +0000

    Bindings Examples

--- code/Chapter2/Lectures/Part1/BindingsLecture/BindingsLecture/MainPage.xaml.cs
+++ code/Chapter2/Lectures/Part1/BindingsLecture/BindingsLecture/MainPage.xaml.cs
@@ -13,9 +13,55 @@ namespace BindingsLecture
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private string _anotherTitle = "This is Label 2";
+        public string AnotherTitle
+        {
+            get => _anotherTitle;
+            set
+            {
+                if (_anotherTitle == value) return;
+                _anotherTitle = value;
+                OnPropertyChanged("AnotherTitle");
+            }
+        }
+
+
+
+        public MainViewModel ViewModel { get; set; }
         public MainPage()
         {
             InitializeComponent();
+            ViewModel = new MainViewModel();
+            this.BindingContext = ViewModel;
+
+            //Set the source object on the target
+            Label2.BindingContext = this;
+            //Apply binding to target
+            Label2.SetBinding(Label.TextProperty, "AnotherTitle");
+
+            //Again, variant 2
+            var binding = new Binding(source: MySlider, path: "Value", stringFormat: "Slider Value: {0:F0}");
+            Label4.SetBinding(Label.TextProperty, binding);
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            //This has no effect
+            MyXamarinConstants.BackgroundCol = new Color(1.0, 0.0, 0.0);
+
+            //This does have an effect
+            TitleLabel.Text = "Button 1 Tapped";
+        }
+
+        void Button2_Clicked(System.Object sender, System.EventArgs e)
+        {
+            //Simply set a property - and see the effect
+            AnotherTitle = "Button 2 Tapped";
+        }
+
+        void Button3_Clicked(System.Object sender, System.EventArgs e)
+        {
+            ViewModel.Greeting = "Button 3 Tapped";
         }
     }
 }
---
+++ code/Chapter2/Lectures/Part1/BindingsLecture/BindingsLecture/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BindingsLecture
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _greeting = "Label 3";
+        public string Greeting {
+            get => _greeting;
+            set
+            {
+                if (value == _greeting) return;
+                _greeting = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand Button4Command { get; set; }
+
+        private void Button4CommandHandler()
+        {
+            Greeting = "Button 4 Tapped";
+        }
+
+        public MainViewModel()
+        {
+            Button4Command = new Command(execute: Button4CommandHandler);
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
