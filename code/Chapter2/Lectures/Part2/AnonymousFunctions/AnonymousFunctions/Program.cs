c6fefcdc62beece283fa5d5d74b1f6785b647993
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2020-02-18 15:50:30 +0000

    Demo of capturing behaviour

---
+++ code/Chapter2/Lectures/Part2/AnonymousFunctions/AnonymousFunctions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AnonymousFunctions
+{
+    class Program
+    {
+        int[] pp = { 1, 3, 5, 7, 9 };
+        int[] qq = { 2, 4, 6, 8, 10 };
+        int[] y = new int[5];
+
+        void Process( Func<int,int,int> f) {
+            for (int n = 0; n < pp.Length; n++) {
+                y[n] = f(pp[n], qq[n]);
+                Console.WriteLine(y[n]);
+            }
+        }
+        int Add(int a, int b) {
+            return a + b;
+        }
+        int Mul(int a, int b) {
+            return a * b;
+        }
+
+        public Program()
+        {
+            //for (int n = 0; n < pp.Length; n++) {
+            //    y[n] = Add(pp[n], qq[n]);
+            //    Console.WriteLine(y[n]);
+            //}
+
+            Process(Add);
+            Process(Mul);
+            //Process( (int a, int b)=> {
+            //    int y = a * (1 + b);
+            //    return y;
+            //} );
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            _ = new Program();
+        }
+    }
+}
---
+++ code/Chapter2/Lectures/Part2/AnonymousFunctions/Capturing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Capturing
+{
+    class Program
+    {
+        Func<int, int> GenerateAccFunc(int initialValue) {
+            int acc = initialValue;
+            return (int u) => {
+                acc += u;
+                return acc;
+            };
+        }
+
+        public Program() {
+            int y1, y2;
+            Func<int, int> accFunc1 = GenerateAccFunc(10);
+            Func<int, int> accFunc2 = GenerateAccFunc(1);
+            accFunc1(2);
+            accFunc2(-5);
+            y1 = accFunc1(3);
+            y2 = accFunc2(10);
+            Console.WriteLine(y1);
+            Console.WriteLine(y2);
+        }
+
+        static void Main(string[] args)
+        {
+            _ = new Program();
+        }
+    }
+}
