c85bb2954e1ec66f892bbd2f9b284312837bf69a
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2020-02-19 09:37:36 +0000

    aditional example of generics

---
+++ code/Chapter2/Lectures/Part2/GenericTypes/GenericTypes/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace GenericTypes
+{
+    public interface ModelTypes
+    {
+        string Author { get; }
+    }
+
+    public class ModelType1 : ModelTypes
+    {
+        public string Name { get; set; } = "Anon";
+        public int Age { get; set; } = 21;
+        public string Author => "Me";
+    }
+
+    public class ModelType2 : ModelTypes
+    {
+        public string Desc { get; set; } = "Square";
+        public double Area { get; set; } = 1.0;
+        public string Author => "You";
+    }
+
+    public class ViewModel1
+    {
+        ModelType1 Model;
+        public ViewModel1( )
+        {
+            Model = new ModelType1();
+        }
+    }
+
+    public class ViewModel2
+    {
+        ModelType2 Model;
+        public ViewModel2()
+        {
+            Model = new ModelType2();
+        }
+    }
+
+    public class ViewModel<T> where T : new()
+    {
+
+        public T Model { get; set; }
+
+        public ViewModel(T m)
+        {
+            Model = m;
+        }
+        public ViewModel()
+        {
+            Model = new T();
+            Console.WriteLine($"{Model}");
+        }
+    }
+
+    public class AnotherViewModel
+    {
+        public ModelTypes Model;
+        public AnotherViewModel(ModelTypes m)
+        {
+            Model = m;
+        }
+        public AnotherViewModel()
+        {
+            //Model = new ????
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ViewModel<ModelType1> m = new ViewModel<ModelType1>();
+            
+        }
+    }
+}
---
+++ code/Chapter2/Lectures/Part2/GenericTypes/SimpleGenerics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SimpleGenerics
+{
+    class Program
+    {
+        public class MyData<T> 
+        {
+            private T[] array;
+            public MyData(int size, Func<int,T> f) 
+            {
+                array = new T[size];
+                for (int n=0; n<size; n++) {
+                    array[n] = f(n);
+                }
+            }
+            public void ListToConsole()
+            {
+                foreach (T element in array)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            MyData<int> d1 = new MyData<int>(10, (int index) => index);
+            d1.ListToConsole();
+            MyData<double> d2 = new MyData<double>(size: 5, (int index) => ((double)index)/5.0);
+            d2.ListToConsole();
+        }
+    }
+}
