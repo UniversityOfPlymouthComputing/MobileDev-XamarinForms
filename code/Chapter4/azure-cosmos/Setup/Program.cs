e25b94485ddc56f11d6cd04cacbcea047311529b
Nicholas Outram <nicholas.outram@plymouth.ac.uk>
2020-03-13 15:28:36 +0000

    UPdates

--- code/Chapter4/azure-cosmos/Setup/Program.cs
+++ code/Chapter4/azure-cosmos/Setup/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Azure.Cosmos;
@@ -43,26 +44,76 @@ namespace Setup
             Console.WriteLine("Created Database: {0}\n", this.database.Id);
 
             //Create container
-            this.container = await this.database.CreateContainerIfNotExistsAsync(containerId, "/IsExplored", 400);
+            this.container = await this.database.CreateContainerIfNotExistsAsync(containerId, "/Orbits", 400);
 
             //Add initial records
-            SolPlanet earth = new SolPlanet("Earth", 400.0, true);
+            await AddPlanetIfDoesNotExist(new SolPlanet("Mercury", 57.0));
+            await AddPlanetIfDoesNotExist(new SolPlanet("Venus", 108.0));
+            await AddPlanetIfDoesNotExist(new SolPlanet("Earth", 150.0, true));
+            await AddPlanetIfDoesNotExist(new SolPlanet("Mars", 228.0));       
+            await AddPlanetIfDoesNotExist(new SolPlanet("Jupiter", 779.0));
+            await AddPlanetIfDoesNotExist(new SolPlanet("Saturn", 1430.0));
+            await AddPlanetIfDoesNotExist(new SolPlanet("Uranus", 2880.0));
+            await AddPlanetIfDoesNotExist(new SolPlanet("Nepture", 4500.0));
+            await AddPlanetIfDoesNotExist(new SolPlanet("Pluto", 5910.0));
 
+            //Read back all records        
+            await QueryAllRecords(true);
+            await QueryAllRecords(false);
+
+            //Update record
+            var mars = new SolPlanet("Mars", 228, true);
+            await ToggleExplored("Mars");
+
+        }
+
+        async Task AddPlanetIfDoesNotExist(SolPlanet p)
+        {
             try
             {
-                // Read the item to see if it exists.  
-                ItemResponse<SolPlanet> earthResponse = await this.container.ReadItemAsync<SolPlanet>(earth.Name, new PartitionKey(earth.IsExplored));
-                Console.WriteLine("Item in database with id: {0} already exists\n", earthResponse.Resource.Name);
+                // Read the item to see if it exists.  The ID (unique) is Name property
+                ItemResponse<SolPlanet> planetResponse = await this.container.ReadItemAsync<SolPlanet>(p.Name, new PartitionKey(p.Orbits));
+                Console.WriteLine("Item in database with id: {0} already exists\n", planetResponse.Resource.Name);
             }
             catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
             {
-                ItemResponse<SolPlanet> earthResponse = await this.container.CreateItemAsync<SolPlanet>(earth, new PartitionKey(earth.IsExplored));
+                ItemResponse<SolPlanet> planetResponse = await this.container.CreateItemAsync<SolPlanet>(p, new PartitionKey(p.Orbits));
 
                 // Note that after creating the item, we can access the body of the item with the Resource property off the ItemResponse. We can also access the RequestCharge property to see the amount of RUs consumed on this request.
-                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", earthResponse.Resource.Name, earthResponse.RequestCharge);
+                Console.WriteLine("Created item in database with id: {0} Operation consumed {1} RUs.\n", planetResponse.Resource.Name, planetResponse.RequestCharge);
             }
+        }
 
+        async Task QueryAllRecords(bool exp)
+        {
+            Console.WriteLine($"Explored is {exp}");
+            var sql = $"SELECT * FROM c where c.IsExplored = {exp.ToString().ToLower()}";
+            Console.WriteLine("Running query: {0}\n", sql);
+            QueryDefinition queryDefinition = new QueryDefinition(sql);
+            FeedIterator<SolPlanet> queryResultSetIterator = this.container.GetItemQueryIterator<SolPlanet>(queryDefinition);
 
+            List<SolPlanet> planets = new List<SolPlanet>();
+
+            while (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<SolPlanet> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                foreach (SolPlanet planet in currentResultSet)
+                {
+                    planets.Add(planet);
+                    Console.WriteLine("\tRead {0}\n", planet);
+                }
+            }
+        }
+
+        //Update
+        async Task ToggleExplored(string name)
+        {
+            ItemResponse<SolPlanet> resp = await this.container.ReadItemAsync<SolPlanet>(name, new PartitionKey("Sol"));
+            SolPlanet itemBody = resp.Resource;
+            itemBody.IsExplored = !itemBody.IsExplored;
+            resp = await container.ReplaceItemAsync<SolPlanet>(itemBody, itemBody.Name, new PartitionKey("Sol"));
+
+            
         }
 
 
--- code/Chapter4/azure-cosmos/Setup/SolPlanet.cs
+++ code/Chapter4/azure-cosmos/Setup/SolPlanet.cs
@@ -8,6 +8,7 @@ namespace Setup
         public string Name { get; set; }
         public double Distance { get; set; }
         public bool IsExplored { get; set; }
+        public string Orbits { get; set; } = "Sol";
         public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
